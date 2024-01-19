3500551c3c895d863d87dff285cf438c3b6e536c
Davor <davor.sehic@gmail.com>
2024-01-19 13:20:04 +0100

    updating html

--- background.js
+++ background.js
@@ -4,7 +4,11 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete' && tab.url.includes('https://www.youtube.com/watch') && tab.url !== lastUrl) {
         // Send a message to the last tab that was on a YouTube video
         lastUrl = tab.url.toString();
-        chrome.tabs.sendMessage(tabId, { type: 'youtube_navigated', msg: Math.random(10) });
+        //chrome.tabs.sendMessage(tabId, { type: 'youtube_navigated', msg: Math.random(10) });
+        chrome.browserAction.setPopup({
+            popup: "popup.html"
+        });
+
         return true;
     }
     return;
---
+++ popup.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const messageElement = document.getElementById("message");
+    messageElement.innerText += "How much of a brainrot is this video? ( 1 - 5 )";
+});
