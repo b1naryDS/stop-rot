f4c429510148ff1101cc1fbf26b911f9182a3016
Davor <davor.sehic@gmail.com>
2024-01-19 10:47:06 +0100

    removed dead code

--- popup.js
+++
@@ -1,9 +0,0 @@
-
-alert("hahah");
-document.addEventListener('DOMContentLoaded', () => {
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'showAlert') {
-      alert('A new YouTube video has started!');
-    }
-  });
-});
