3f489afd9971eb06a2beadccd447813ebac67565
Davor <davor.sehic@gmail.com>
2024-01-19 15:28:07 +0100

    cleanup

--- content.js
+++
@@ -1,6 +0,0 @@
-// content.js
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.type === 'youtube_navigated') {
-        alert('You have navigated to a new YouTube video!'+ request.msg);
-    }
-});
--- popup.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const messageElement = document.getElementById("message");
-    messageElement.innerText += "How much of a brainrot is this video? ( 1 - 5 )";
-});
